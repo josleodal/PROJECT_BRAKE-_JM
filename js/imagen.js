7547df262711e7d09d83a9b18974f94721bae344
josleodal <148706231+josleodal@users.noreply.github.com>
2023-12-22 21:28:31 +0100

    Update imagen.js

--- js/imagen.js
+++ js/imagen.js
@@ -1,15 +1,15 @@
 function cambiarImagen() {
     const imagenes = [
-        'url("assets/balance.jpg")',
-        'url("assets/beach.jpg")',
-        'url("assets/buddhism.jpg")',
-        'url("assets/lake.jpg")',
-        'url("assets/landscape.jpg")',
-        'url("assets/ocean.jpg")',
-        'url("assets/oilseed.jpg")',
-        'url("assets/seascape.jpg")',
-        'url("assets/stones.jpg")',
-        'url("assets/wood.jpg")'
+        'url("./assets/balance.jpg")',
+        'url("./assets/beach.jpg")',
+        'url("./assets/buddhism.jpg")',
+        'url("./assets/lake.jpg")',
+        'url("./assets/landscape.jpg")',
+        'url("./assets/ocean.jpg")',
+        'url("./assets/oilseed.jpg")',
+        'url("./assets/seascape.jpg")',
+        'url("./assets/stones.jpg")',
+        'url("./assets/wood.jpg")'
     ];
 
      
@@ -17,7 +17,7 @@ function cambiarImagen() {
        
       
     
-      if (!document.body.style) {
+      if (!document.body.style.backgroundImage) {
         document.body.style.backgroundImage = imagenes[0];
         document.body.style.backgroundRepeat= no-repeat
         document.body.style.backgroundSize= cover;
