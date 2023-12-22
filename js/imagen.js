0d2c4ee26ec88f2cbab5c2ba2703bfeb5d8be5c1
josleodal <josleodal@gmail.com>
2023-12-22 20:57:52 +0100

    subida

--- js/imagen.js
+++ js/imagen.js
@@ -1,20 +1,19 @@
 function cambiarImagen() {
     const imagenes = [
-        'url("assets/balance.jpg")',
-        'url("assets/beach.jpg")',
-        'url("assets/lake.jpg")',
-        'url("assets/buddhism.jpg")',
-        'url("assets/landscape.jpg")',
-        'url("assets/ocean.jpg")',
-        'url("assets/oilseed.jpg")',
-        'url("assets/seascape.jpg")',
-        'url("assets/stones.jpg")',
-        'url("assets/beach.jpg")',
-        'url("assets/wood.jpg")'
+        'url("https://github.com/josleodal/PROJECT_BRAKE-_JM/blob/main/assets/balance.jpg")',
+        'url("https://github.com/josleodal/PROJECT_BRAKE-_JM/blob/main/assets/beach.jpg")',
+        'url("https://github.com/josleodal/PROJECT_BRAKE-_JM/blob/main/assets/buddhism.jpg")',
+        'url("https://github.com/josleodal/PROJECT_BRAKE-_JM/blob/main/assets/lake.jpg")',
+        'url("https://github.com/josleodal/PROJECT_BRAKE-_JM/blob/main/assets/landscape.jpg")',
+        'url("https://github.com/josleodal/PROJECT_BRAKE-_JM/blob/main/assets/ocean.jpg")',
+        'url("https://github.com/josleodal/PROJECT_BRAKE-_JM/blob/main/assets/oilseed.jpg")',
+        'url("https://github.com/josleodal/PROJECT_BRAKE-_JM/blob/main/assets/seascape.jpg")',
+        'url("https://github.com/josleodal/PROJECT_BRAKE-_JM/blob/main/assets/stones.jpg")',
+        'url("https://github.com/josleodal/PROJECT_BRAKE-_JM/blob/main/assets/wood.jpg")'
     ];
 
-    
-    if (!document.body.style.backgroundImage) {
+     
+     if (!document.body.style.backgroundImage) {
         document.body.style.backgroundImage = imagenes[0];
     } else {
        
