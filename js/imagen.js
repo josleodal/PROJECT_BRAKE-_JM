3cbf9dfc0bb90274554054bd770897e54e344aa5
josleodal <148706231+josleodal@users.noreply.github.com>
2023-12-22 20:37:12 +0100

    Update imagen.js

--- js/imagen.js
+++ js/imagen.js
@@ -12,13 +12,11 @@ function cambiarImagen() {
         'url("assets/wood.jpg")'
     ];
 
-     if (!document.body.style.backgroundImage) {
-        document.body.style.backgroundImage = imagenes[0];
-    } else {
+   
        
         const randomImagen = imagenes[Math.floor(Math.random() * imagenes.length)];
         document.body.style.backgroundImage = randomImagen;
-    }
+    
 }
 
 setInterval(cambiarImagen, 15000);
